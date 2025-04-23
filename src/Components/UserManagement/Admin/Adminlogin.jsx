204b50cac3a9e468c93cdc5be190cad77c5ac9a1
sidNet9899 <sgotmare@netfotech.in>
2025-04-23 13:13:26 +0530

    updateeddd

--- src/Components/UserManagement/Admin/Adminlogin.jsx
+++ src/Components/UserManagement/Admin/Adminlogin.jsx
@@ -15,7 +15,7 @@ export default function AdminLogin() {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:5000/api/admin/login", {
+      const response = await fetch("https://e-commerce-backend-phi-five.vercel.app/api/admin/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
