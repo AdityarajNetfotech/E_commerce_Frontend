ead637863c2dc0d877d714a87100bfd94bedaeff
sidNet9899 <sgotmare@netfotech.in>
2025-04-23 11:11:16 +0530

    update 3

--- src/Pages/Student/ProductPages/Product.jsx
+++ src/Pages/Student/ProductPages/Product.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './header/Header'; 
 import SearchBar from './search-bar/SearchBar';
 import ProductGrid from './product/ProductGrid';
-import Footer from '../../../Components/footer/Footer';
+import Footer from '../../../Components/Footer/Footer';
 import Navbar from '../../../Components/Navbar/Navbar'
 
 function Product() {
